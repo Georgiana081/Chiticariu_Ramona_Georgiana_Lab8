526d9ce764579b06a05eb2278969680faab54d42
RAMONA-GEORGIANA CHITICARIU <ramona.chiticariu@stud.ubbcluj.ro>
2021-11-28 16:06:36 +0200

    Add project files.

---
+++ Data/Chiticariu_Ramona_Georgiana_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Chiticariu_Ramona_Georgiana_Lab8.Models;
+
+namespace Chiticariu_Ramona_Georgiana_Lab8.Data
+{
+    public class Chiticariu_Ramona_Georgiana_Lab8Context : DbContext
+    {
+        public Chiticariu_Ramona_Georgiana_Lab8Context (DbContextOptions<Chiticariu_Ramona_Georgiana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Chiticariu_Ramona_Georgiana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Chiticariu_Ramona_Georgiana_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Chiticariu_Ramona_Georgiana_Lab8.Models.Category> Category { get; set; }
+    }
+}
