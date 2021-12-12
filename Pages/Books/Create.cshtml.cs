68b9c1b3929a2955d1ff4c1987a74f1d13e538ea
RAMONA-GEORGIANA CHITICARIU <ramona.chiticariu@stud.ubbcluj.ro>
2021-12-12 19:33:46 +0200

    Laborator 9

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -10,7 +10,7 @@ using Chiticariu_Ramona_Georgiana_Lab8.Models;
 
 namespace Chiticariu_Ramona_Georgiana_Lab8.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Chiticariu_Ramona_Georgiana_Lab8.Data.Chiticariu_Ramona_Georgiana_Lab8Context _context;
 
@@ -22,11 +22,43 @@ namespace Chiticariu_Ramona_Georgiana_Lab8.Pages.Books
         public IActionResult OnGet()
         {
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+            var book = new Book();
+            book.BookCategories = new List<BookCategory>();
+            PopulateAssignedCategoryData(_context, book);
             return Page();
         }
 
         [BindProperty]
         public Book Book { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
+        {
+            var newBook = new Book();
+            if (selectedCategories != null)
+            {
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
+            }
+            if (await TryUpdateModelAsync<Book>(
+            newBook,
+            "Book",
+            i => i.Title, i => i.Author,
+            i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+            {
+                _context.Book.Add(newBook);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            PopulateAssignedCategoryData(_context, newBook);
+            return Page();
+        }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
---
+++ hiticariu_Ramona_Georgiana_Lab8/Context.cs
@@ -0,0 +1,6 @@
+﻿namespace hiticariu_Ramona_Georgiana_Lab8
+{
+    public class Context
+    {
+    }
+}
